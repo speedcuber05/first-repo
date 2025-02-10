fffdfc44e6aa70e0d5222ce1074adb49c8c0c9ea
Devansh Mehrotra <Devanshmehrotra2005@gmail.com>
2025-02-11 03:30:56 +0530

    more plotting

---
+++ python/matplotlib/pltsubplots.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(123)
+d1,d2,d3,d4=np.random.randn(4,100)
+fig,ax=plt.subplots(2,2,figsize=(6.4,4.8),facecolor="green",sharex=True,sharey=False,constrained_layout=True,layout="tight") 
+ax[0,0].scatter(d1,d2,s=10,marker="x")
+ax[0,1].scatter(d1,d2,s=10,marker="d")
+print("showing graph")
+plt.show()
+print("done")
+
+
+#sharex and sharey basically means there will only be 1 label
+#constrained_layout=True makes sure there is no overlapping of texts and stuff
+#layout are just different layouts, negligible difference in simple graphs
+
+
+#matplotlib.pyplot.subplots(nrows=1, ncols=1, *, sharex=False, sharey=False, squeeze=True, width_ratios=None,
+#height_ratios=None, subplot_kw=None, gridspec_kw=None, **fig_kw)
--- python/matplotlib/simpleplotting.py
+++ python/matplotlib/simpleplotting.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3],[4,9,6])
+plt.plot([1,2,3,7,4,3,6],[4,9,6,2,4,1,4])
 plt.ylim(0,10)
 plt.xlim(0,10)
 plt.show()

864a05a5c788c0d7c12ce2f65e3080ab96bf8579
Devansh Mehrotra <Devanshmehrotra2005@gmail.com>
2025-02-10 18:31:28 +0530

    starting with matplotlib

---
+++ python/matplotlib/plotlines.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3],[4,9,6])
+plt.ylim(0,10)
+plt.xlim(0,10)
+plt.show()
+print("done")
+
+#a figure and an axis is automatically created here
